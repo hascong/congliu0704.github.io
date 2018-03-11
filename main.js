8173ab6af2f72838199b62d037c2c80592ac2d53
congliu0704 <congliu0704@gmail.com>
2018-03-12 03:04:10 +0800

    Changed to double quote

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 <script>
-const btn = document.getElementById('like')
-btn.addEventListenser('click', function() {
+const btn = document.getElementById("like")
+btn.addEventListenser("click", function() {
   alert('Thank you.');
 });
 </script>
